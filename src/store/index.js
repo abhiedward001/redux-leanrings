61981aa7798b7e4562a5475f5fe464c5e00c0e21
Abhishek Jaiswal <iam001abhi@gmail.com>
2024-10-27 12:30:38 +0530

    feat: store configuration and subscribe with counter component

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore } from 'redux';
+
+const reducer = (state = { counter: 0 }, action) => {
+  switch (action.type) {
+    case 'increment':
+      return {
+        counter: state.counter + 1
+      }
+    case 'decrement':
+      return {
+        counter: state.counter - 1
+      }
+    default:
+      return state;
+  }
+
+}
+const store = createStore(reducer);
+
+
+export default store;
